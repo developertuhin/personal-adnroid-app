b70e5abfaef4247d291d65cc0aaf5a481d692005
Tuhin Khan <webwithjhankarvai@gmail.com>
2021-06-17 09:14:21 +0600

    Personal Android App

---
+++ README.md
@@ -0,0 +1,1 @@
+# personal-adnroid-app
---
+++ app/src/main/java/com/tuhin/developertuhin/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tuhin.developertuhin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+        WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://determined-hamilton-cba9f7.netlify.app/");
+
+
+
+    }
+}
